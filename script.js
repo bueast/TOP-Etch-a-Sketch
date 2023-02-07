6c5fd50982927f7017ddaec1d91ea9c3d38acd3a
bueast <billyrobertschef@gmail.com>
2023-02-08 04:02:42 +1300

    Added color picker

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const screen = document.querySelector('.screen');
+let selectedColor = 'black';
 
 for (let i = 0; i < 256; i++) {
-      const gridBox = document.createElement('div');
-      gridBox.classList.add('grid-box');
-      screen.appendChild(gridBox);
+  const gridBox = document.createElement('div');
+  gridBox.classList.add('grid-box');
+  screen.appendChild(gridBox);
 
-      gridBox.addEventListener('mouseenter', () => {
-            gridBox.style.backgroundColor = 'black';
-      });
+  gridBox.addEventListener('mouseenter', (event) => {
+    event.target.style.backgroundColor = selectedColor;
+  });
 }
 
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', () => {
-      const gridBoxes = document.querySelectorAll('.grid-box');
-      gridBoxes.forEach(gridBox => {
-            gridBox.style.backgroundColor = 'blueviolet';
-      });
+  const gridBoxes = document.querySelectorAll('.grid-box');
+  gridBoxes.forEach(gridBox => {
+    gridBox.style.backgroundColor = 'blueviolet';
+  });
 });
 
 document.body.appendChild(resetButton);
 
+const colorPicker = document.querySelector("#color-picker");
 
-
-
-
+colorPicker.addEventListener("input", function() {
+  selectedColor = this.value;
+});
