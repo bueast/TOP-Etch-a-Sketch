fa85c68c7bb19bea0fa0088527bd3d3bef818088
bueast <billyrobertschef@gmail.com>
2023-02-08 03:38:29 +1300

    Working with reset button. Need to add js for other 2 buttons

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const screen = document.querySelector('.screen');
-    for (let i = 0; i < 256; i++) {
+
+for (let i = 0; i < 256; i++) {
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
-      screen.appendChild(gridBox);}
+      screen.appendChild(gridBox);
+
+      gridBox.addEventListener('mouseenter', () => {
+            gridBox.style.backgroundColor = 'black';
+      });
+}
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', () => {
+      const gridBoxes = document.querySelectorAll('.grid-box');
+      gridBoxes.forEach(gridBox => {
+            gridBox.style.backgroundColor = 'blueviolet';
+      });
+});
+
+document.body.appendChild(resetButton);
+
+
+
+
+
